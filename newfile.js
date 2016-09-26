12a7e1d3317987d2c366fe86dbb7adbd8dd658d2
Weiyif Zhang <weiyifzhang@weiyifs-mbp.bmcc.cuny.edu>
2016-09-26 16:48:49 -0400

    first

---
+++ newfile.js
@@ -0,0 +1,12 @@
+ <script type="text/javascript">
+        function updateMessage(){
+            var message = document.getElementById('message');
+            message.innerHTML = "We're so happy you're here!";
+        }
+        
+        alert('Into JavaScript');
+    </script> 
+
+/**
+ * @author Weiyif Zhang
+ */
